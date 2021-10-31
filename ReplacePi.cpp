e09aef503a08b1321381b005f085e9029f7b5dee
Akhil870 <72069148+Akhilesh0947@users.noreply.github.com>
2021-10-31 17:25:32 +0530

    Create ReplacePi.cpp

---
+++ ReplacePi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void fun(string s)
+{
+    if (s.length()==0){
+        return;
+    }
+
+    if (s[0]=='p' && s[1]=='i'){
+        cout<<"3.14";
+        fun(s.substr(2));
+    }
+    else{
+	    cout<<s[0];
+	    fun(s.substr(1));
+	}  
+}
+int main() {
+   string s="pippxxppixipi";
+   fun(s);
+   return 0;
+}
